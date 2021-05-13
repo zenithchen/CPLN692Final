ab6f25906be2b3e31482ec421241163e27fb9270
zenithchen <512211275@qq.com>
2021-05-14 01:59:52 +0800

    add Ajax loading spinner

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,28 @@
 /* =====================
   Global Variables
 ===================== */
-var data;  // for holding data
+var parcelURL;
+var nearbyURL;
+
+var parceldata;  // for holding data
 var censusData;
+var nearby_data;
+
 var marker;
 var parcel_geo;
-var nearby_data;
+var parcel_layer;
 var nearby_marker_lst=[];
 var addr;
+var nearby_addr;
+
+var zoning;
+var category;
+var vio_code;
+var year_built;
+var total_area;
+var story;
+var room;
+var frontage;
 
 var redIcon = new L.Icon({
   iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
@@ -19,7 +34,7 @@ var redIcon = new L.Icon({
 });
 
 var blueIcon = new L.Icon({
-  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png',
+  iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-grey.png',
  // shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
   iconSize: [15, 28],
   iconAnchor: [12, 41],
@@ -30,10 +45,32 @@ var blueIcon = new L.Icon({
 /* =====================
   Functions
 ===================== */
+var newapi = function(input){
+  var address = `http://3.22.171.167:8000/parcel_info?addr=${input}`
+   console.log(address)
+  // return(encodeURI(address))
+  return(address)
+}
+
+var updateapi = function(api){
+  $('#apilink').html(`<button type="button" class="btn btn-lg btn-danger" data-toggle="popover" 
+  title="IP Address" data-bs-content="<a href='${api}' target='_blank'> ${api} </a>">API</button>`)
+}
+
 var removeMarkers = function(lst) {
   lst.forEach((item) =>{
       map.removeLayer(item);
       })
+};
+
+var removeGeometry = function() {
+  if (marker != undefined) {
+    map.removeLayer(marker);
+    removeMarkers(nearby_marker_lst);
+  };
+  if(parcel_geo != undefined) {
+    map.removeLayer(parcel_layer);
+  }
 };
 
 var plotMarkers = function(lst) {
@@ -42,89 +79,185 @@ var plotMarkers = function(lst) {
   })
 };
 
-function onClick2(e) {
-  // L.DomEvent.stopPropagation(e);
-  // alert(this.getLatLng());
-  alert("click function!")
+function updateChart(barchart, newdata){
+  barchart.data.datasets[0].data[0] = newdata
+  barchart.update()
 }
+
+function setMarkers(dataArr){
+  // console.log(dataArr)
+  removeGeometry();
+  // removeMarkers(nearby_marker_lst);
+  nearby_marker_lst=[];
+  parcel_geo = dataArr.parcel_geometry[0].geometry;
+  var lat = parseFloat(dataArr.parcel_df[0].Parcel_centroid_lat);
+  var lng = parseFloat(dataArr.parcel_df[0].Parcel_centroid_lng);
+  addr = dataArr.parcel_df[0].Input.replaceAll('%20', ' ')
+  marker = L.marker([lat, lng],{icon: redIcon}).bindPopup(addr);
+
+  dataArr.nearby_parcel_df.forEach((item) =>{
+    var myMarker = L.marker([item.LAT, item.LNG],{icon: blueIcon}).bindPopup(item.ADDR_SOURCE).on('click', onClick);
+    nearby_marker_lst.push(myMarker);
+    })
+}
+
+//Cards: 311 Request
+var new311 = function(entry){
+  if(entry.length>1){
+   return (`${entry[0]}<br/>${entry[1]}<br/>...`)
+  }else if(entry.length==1){
+   return(`${entry[0]}<br/>...`)
+   }else{
+    return(`none`)}
+   }
+
+var update311 =function(request){
+  let count311= request.length
+  var names311=[]
+  for(let i=0;i<count311;i++){
+    name311 = request[i].service_name
+    names311.push(name311)
+  }
+  $('#311count').html(count311)
+  $('#311name').html(new311(names311))
+}
+
+//Cards: Nearby Parcel
+var newparcel = function(entry){
+  if(entry.length>1){
+   return (`${entry[0]}<br/>${entry[1]}<br/>...`)
+  }else if(entry.length==1){
+   return(`${entry[0]}<br/>...`)
+   }else{
+    return(`none`)}
+   }
+
+var updateparcel= function(dataArr){
+  var countparcel= dataArr.length
+  var namesparcel=[]
+  for(let i=0;i<countparcel;i++){
+    nameparcel = dataArr[i].ADDR_SOURCE
+    namesparcel.push(nameparcel)
+  }
+  $('#parcelcount').html(countparcel)
+  $('#parcelname').html(newparcel(namesparcel))
+}
+
+
+function plotElements(){
+  var markerBounds = L.latLngBounds([marker.getLatLng()]);
+  map.fitBounds(markerBounds);
+
+  parcel_layer = L.geoJson(parcel_geo,{
+    style: {color: "orange", weight: 3}
+  }).addTo(map);
+
+  //add markers
+  plotMarkers(nearby_marker_lst);
+  marker.addTo(map).openPopup();
+}
+
+var updaterisk= function(risk){
+  if (parceldata.prediction[0].Relative_risk === 'Above average'){
+    $(".above").html("Above")
+  }else if(parceldata.prediction[0].Relative_risk === 'Below average'){
+    $(".above").html("Below")
+  }
+}
+
+function getInfo(dataArr){
+  zoning = dataArr.properties_df[0].zoning;
+  category = dataArr.properties_df[0].category;
+  vio_code = dataArr.properties_df[0].vio_title;
+  year_built = dataArr.properties_df[0].year_built;
+  total_area = dataArr.properties_df[0].total_area;
+  story = dataArr.properties_df[0].number_stories;
+  room = dataArr.properties_df[0].number_of_rooms;
+  frontage = dataArr.properties_df[0].frontage;
+  request = dataArr.request311_within100m
+  nearby = dataArr.nearby_parcel_df
+  risk = dataArr.prediction[0].Relative_risk
+}
+
+/*click nearby marker function*/ 
+function onClick(e) {
+  $('#loader').show()
+  var popup = this.getPopup();
+  nearby_addr = popup.getContent();
+  nearbyURL = newapi(nearby_addr)
+
+  $.ajax({
+    async: false,
+    url: nearbyURL ,
+    dataType: 'json',
+    headers:{'Access-Control-Allow-Origin':'*'}
+  }).done(function(nearbyRes){
+    nearby_data = nearbyRes
+  });
+
+  $('#loader').hide()
+  console.log(nearby_data)
+
+  setMarkers(nearby_data);
+  plotElements();
+  getInfo(nearby_data);
+  updateChart(area_Chart, total_area);
+  updateChart(frontage_Chart, frontage);
+  updateChart(room_Chart, room);
+  update311(request)
+  updateparcel(nearby)
+}
+
 
 
 /* =====================
   Parse and store data for later use
 ===================== */
-
-//  var parcelURL = "https://raw.githubusercontent.com/zenithchen/CPLN692Final/main/Data/fixedResponse0427.json"
-var parcelURL = "https://raw.githubusercontent.com/zenithchen/CPLN692Final/main/Data/response_0505.json"
 var censusURL = "https://raw.githubusercontent.com/zenithchen/CPLN692Final/main/Data/Census_Tracts_2010.geojson"
-var nearbyparcelURL = "https://raw.githubusercontent.com/zenithchen/CPLN692Final/main/Data/response05052.json"
 
 $(document).ready(function() {
+  $('#loader').hide();
 
-  $.when($.ajax(censusURL),$.ajax(parcelURL)).then(function(censusRes, parcelRes){
-    censusData = JSON.parse(censusRes[0]);
-    data = JSON.parse(parcelRes[0]);
-    // nearby_data = 
-    parcel_geo = data.parcel_geometry[0].geometry;
-    // console.log(parcel_geo)
-   
-    var lat = parseFloat(data.parcel_df[0].Parcel_centroid_lat);
-    var lng = parseFloat(data.parcel_df[0].Parcel_centroid_lng);
-    addr = data.parcel_df[0].Input.replaceAll('%20', ' ')
-    // console.log(data.parcel_geometry[0].geometry.coordinates[0])
+  $('#btnGroupAddon').click(function() {
+    $('#loader').show()
+    var inputAddr = $('.form-control').val();
+    parcelURL = newapi(inputAddr);
 
-    L.geoJson(censusData).addTo(map)
-    marker = L.marker([lat, lng],{icon: redIcon}).bindPopup(addr).on('click', onClick2);
-    // marker.on('click', onClick);
-    // console.log(marker)
-    
-    //console.log(data.nearby_parcel_df[0].LAT)
-    data.nearby_parcel_df.forEach((item) =>{
-      var myMarker = L.marker([item.LAT, item.LNG],{icon: blueIcon}).bindPopup(item.ADDR_SOURCE).on('click', onClick2);
-      nearby_marker_lst.push(myMarker);
-      })
-    //console.log(nearby_marker_lst)
+    $.ajax({
+      async: false,
+      url:parcelURL,
+      dataType: 'json',
+      headers:{'Access-Control-Allow-Origin':'*'}
+    }).done(function(parcelRes){
+      parceldata= parcelRes
+    });
 
-    //L.marker([lat, lng]).bindPopup(add).addTo(map);
-  })
+    $('#loader').hide()
+    console.log(parceldata)
 
-  $('#search-btn').click(function() {
-    if (marker != undefined) {
-      map.removeLayer(marker);
-    };
-    removeMarkers(nearby_marker_lst);
-
-    var inputAddr = $('#input-addr').val();
+    setMarkers(parceldata);
 
     if(inputAddr===addr){
-      var markerBounds = L.latLngBounds([marker.getLatLng()]);
-      map.fitBounds(markerBounds);
-      
-      var poly = L.geoJson(parcel_geo,{
-        // style: {color: 'red'}
-      })
-      poly.addTo(map);
-      console.log(poly)
+      plotElements();
 
-      // L.geoJson(parcel_geo,{
-      //   // style: {color: 'red'}
-      // }).addTo(map).on('click', onClick);
-      plotMarkers(nearby_marker_lst);
-      marker.addTo(map).openPopup();
-      
-      var zoning = data.properties_df[0].zoning;
-      var category = data.properties_df[0].category;
-      var vio_code = data.properties_df[0].vio_title;
-      var year_built = data.properties_df[0].year_built;
-      var total_area = data.properties_df[0].total_area;
-      var story = data.properties_df[0].number_stories;
+      getInfo(parceldata);
 
-      $('#tb-zoning').text(zoning);
-      $('#tb-cat').text(category);
-      $('#tb-vio').text(vio_code);
-      $('#tb-year').text(year_built);
-      $('#tb-area').text(total_area);
-      $('#tb-story').text(story);
-      // $('#exampleModal2').modal('show');
+      updateChart(area_Chart, total_area);
+      updateChart(frontage_Chart, frontage);
+      updateChart(room_Chart, room);
+      update311(request)
+      updateparcel(nearby)
+      updaterisk(risk)
+      //api popover
+      var api = newapi(inputAddr);
+      updateapi(api)
+      $(function () {
+        $('[data-toggle="popover"]').popover({
+           trigger: 'click',
+           sanitize : false,
+           html:true
+          })
+      })     
     }
     else{
       alert("NONE FOUND")
@@ -132,4 +265,3 @@ $(document).ready(function() {
   });
 
 })
-
